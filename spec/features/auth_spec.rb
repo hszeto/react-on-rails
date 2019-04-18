fe925299b736a61d23db9dfae0ff4c71c454d775
Henry Szeto <henryszeto@gmail.com>
2019-04-18 01:25:20 -0700

    add feature spec with rspec-rails, capybara, selenium-webdriver, capybara-selenium, chromedriver-helper

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe 'Auth', type: :feature, js: true do
+  before do
+    visit '/'
+  end
+
+  context 'When Not Login' do
+    it 'visit Home' do
+      expect(page).to have_content 'Home'
+    end
+
+    it 'visit Public' do
+      click_link 'Public'
+
+      expect(page).to have_content 'This is public page. No secret here.'
+    end
+
+    it 'visit Private' do
+      click_link 'Private'
+
+      expect(page).to have_content 'Login first'
+    end
+  end
+
+  context 'When Login' do
+    before do
+      click_button 'Login'
+    end
+
+    it 'visit Home' do
+      expect(page).to have_content 'Home'
+    end
+
+    it 'visit Public' do
+      click_link 'Public'
+
+      expect(page).to have_content 'This is public page. No secret here.'
+    end
+
+    it 'visit Private' do
+      click_link 'Private'
+
+      expect(page).to have_content 'You have logged in. The secret is, there is no secret.'
+    end
+  end
+
+  context 'When Login then Logout' do
+    before do
+      click_button 'Login'
+      sleep 1
+      click_button 'Logout'
+    end
+
+    it 'visit Private' do
+      click_link 'Private'
+
+      expect(page).to have_content 'Login first'
+    end
+  end
+end
